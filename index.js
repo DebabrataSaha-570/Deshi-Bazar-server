31e0fc0ca813d3c7b2bd3e24c5bd612cddb9a98d
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2024-03-23 16:22:23 +0600

    create , read and get single product api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -23,10 +23,49 @@ async function run() {
     await client.connect();
     console.log("Connected to MongoDB");
 
-    const db = client.db("authentication");
-    const collection = db.collection("users");
+    const db = client.db("deshiBazar");
+    const productsCollection = db.collection("products");
 
-    // User Login
+    app.post("/api/v1/product", async (req, res) => {
+      const product = req.body;
+      console.log("product", product);
+      const result = await productsCollection.insertOne(product);
+      res.json(result);
+    });
+
+    app.get("/api/v1/products", async (req, res) => {
+      try {
+        let query = {};
+
+        if (req.query.categories) {
+          query.categories = req.query.categories;
+        } else if (req.query.flash_sale) {
+          query.flash_sale = req.query.flash_sale;
+        }
+        const products = productsCollection.find(query);
+        const result = await products.toArray();
+        res.json(result);
+      } catch (error) {
+        console.log("error", error);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
+
+    app.get("/api/v1/product/:productId", async (req, res) => {
+      const id = req.params.productId;
+      try {
+        const query = { _id: new ObjectId(id) };
+        const result = await productsCollection.findOne(query);
+        if (result) {
+          res.json(result);
+        } else {
+          res.status(404).json({ error: "Product Not Found" });
+        }
+      } catch (error) {
+        console.error("Error ", error);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
 
     // Start the server
     app.listen(port, () => {
